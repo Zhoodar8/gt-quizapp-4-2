ace59eaa47c8ddf572cf6d0cb21b66be1c5c3bcd
zhoodar8 <madinah312>
2020-01-25 17:39:54 +0600

    Изменил Имена во Фрагментах

--- app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainFragment.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.geektech.quizapp_gt_4_2.R;
@@ -26,6 +27,7 @@ public class MainFragment extends CoreFragment {
     }
 
     private MainViewModel mViewModel;
+    private SeekBar seekBar;
     public static MainFragment newInstance() {
         return new MainFragment();
     }
@@ -34,6 +36,10 @@ public class MainFragment extends CoreFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        seekBar = view.findViewById(R.id.seek_bar);
+        seekBar.setProgress(0);
+        seekBar.setMax(50);
+        seekBar.setPadding(21,0,21,0);
 
     }
 
