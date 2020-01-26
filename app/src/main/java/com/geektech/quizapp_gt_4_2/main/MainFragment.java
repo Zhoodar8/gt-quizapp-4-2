52758df6d64341451453d0ab7051d4d76304b422
zhoodar8 <madinah312>
2020-01-26 22:56:00 +0600

    Добавил связь между SeekBar и TextView

--- app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainFragment.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainFragment.java
@@ -3,6 +3,7 @@ package com.geektech.quizapp_gt_4_2.main;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.geektech.quizapp_gt_4_2.R;
@@ -28,6 +30,7 @@ public class MainFragment extends CoreFragment {
 
     private MainViewModel mViewModel;
     private SeekBar seekBar;
+    private TextView textQuestionCount;
     public static MainFragment newInstance() {
         return new MainFragment();
     }
@@ -37,9 +40,27 @@ public class MainFragment extends CoreFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         seekBar = view.findViewById(R.id.seek_bar);
+        textQuestionCount = view.findViewById(R.id.txt_q_count);
         seekBar.setProgress(0);
         seekBar.setMax(50);
         seekBar.setPadding(21,0,21,0);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                     textQuestionCount.setText(""+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
