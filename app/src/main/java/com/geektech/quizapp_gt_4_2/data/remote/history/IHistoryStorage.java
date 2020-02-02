c92ffa077b62a316c5d83ae2a9d9a87e6db76465
zhoodar8 <madinah312>
2020-02-02 22:29:21 +0600

    Добавил Примерные методы в IHistoryStorage

--- app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/IHistoryStorage.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/IHistoryStorage.java
@@ -1,4 +1,10 @@
 package com.geektech.quizapp_gt_4_2.data.remote.history;
 
 public interface IHistoryStorage {
+
+    void insert();
+
+    void delete();
+    
+    void deleteAll();
 }
--- app/src/main/java/com/geektech/quizapp_gt_4_2/result/ResultActivity.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/result/ResultActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.geektech.quizapp_gt_4_2.R;
 import com.geektech.quizapp_gt_4_2.core.CoreActivity;
@@ -16,14 +18,27 @@ public class ResultActivity extends CoreActivity {
         return R.layout.activity_result;
     }
 
+    private TextView tvResultTittle;
+    private TextView tvResultDifficulty;
+    private TextView tvResultScore;
+    private TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tvResultTittle =findViewById(R.id.card_tittle);
+        tvResultDifficulty = findViewById(R.id.card_diff_all);
+        tvResultScore = findViewById(R.id.card_score);
+        tvResult = findViewById(R.id.card_result_percent);
+
 
     }
 
     public static void startResult(Context context){
         Intent intent = new Intent(context, ResultActivity.class);
         context.startActivity(intent);
+    }
+
+    public void onClickFinish(View view) {
     }
 }
