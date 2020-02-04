540734db6b0fed5ad856953683bdd42b77dcc71a
zhoodar8 <madinah312>
2020-02-04 11:48:15 +0600

    Закончил верстку Settings fragment

--- app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/HistoryStorage.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/HistoryStorage.java
@@ -2,9 +2,10 @@ package com.geektech.quizapp_gt_4_2.data.remote.history;
 
 public class HistoryStorage implements IHistoryStorage {
 
-    @Override
-    public void insert() {
 
+    @Override
+    public void save() {
+        
     }
 
     @Override
@@ -14,6 +15,16 @@ public class HistoryStorage implements IHistoryStorage {
 
     @Override
     public void deleteAll() {
+
+    }
+
+    @Override
+    public void get() {
+
+    }
+
+    @Override
+    public void getAll() {
 
     }
 }
--- app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/IHistoryStorage.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/data/remote/history/IHistoryStorage.java
@@ -1,21 +1,24 @@
 package com.geektech.quizapp_gt_4_2.data.remote.history;
 
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
 
-@Dao
+
+
 public interface IHistoryStorage {
 
 
-    @Insert
-    void insert();
+
+    void save();
 
 
-    @Delete
+
     void delete();
 
 
-    @Delete
+
     void deleteAll();
+
+
+    void get();
+
+    void getAll();
 }
